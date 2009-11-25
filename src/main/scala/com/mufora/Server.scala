e80af21b5600956f0b3160cadb5c8547de530266
Toby Jungen <toluju@gmail.com>
2009-11-25 00:33:32 -0600

    Some cleanups

--- src/main/scala/com/mufora/Server.scala
+++ src/main/scala/com/mufora/Server.scala
@@ -18,6 +18,7 @@ import javax.servlet.ServletContext
 import javax.ws.rs.core.Context
 import com.sun.jersey.api.view.Viewable
 import scala.collection.{ mutable }
+import org.scala_tools.javautils.Imports._
 
 
 @Path("/")
@@ -104,13 +105,9 @@ class FreemarkerTemplateProvider extends TemplateProcessor {
 }
 
 class ScalaBeansWrapper extends SimpleObjectWrapper {
-  def wrapByParent(obj: AnyRef) = super.wrap(obj)
-
   override def wrap(obj: Object): TemplateModel = {
     obj match {
-      case smap: scala.collection.Map[_,_] => super.wrap(JMap(smap))
-      case sseq: scala.Seq[_] => super.wrap(new JList(sseq))
-      case scol: scala.Collection[_] => super.wrap(JCollection(scol))
+      case scol: scala.Collection[_] => super.wrap(scol.asJava)
       case sobj: ScalaObject => new ScalaHashModel(this, sobj)
       case _ => super.wrap(obj)
     }
@@ -118,7 +115,9 @@ class ScalaBeansWrapper extends SimpleObjectWrapper {
 }
 
 /** A model that will expose all Scala getters that has zero parameters
- * to the FM Hash#get method so can retrieve it without calling with parenthesis. */
+ * to the FM Hash#get method so can retrieve it without calling with parenthesis.
+
+ This stuff is still kinda magic to me... -TJ */
 class ScalaHashModel(wrapper: ObjectWrapper, sobj: ScalaObject) extends TemplateHashModel {
   type Getter = () => AnyRef
 
@@ -134,7 +133,7 @@ class ScalaHashModel(wrapper: ObjectWrapper, sobj: ScalaObject) extends Template
           cls.getMethods.foreach { m =>
             val n = m.getName
             if(!n.endsWith("_$eq") && m.getParameterTypes.length==0){
-              map += Pair(n, (() => m.invoke(sobj, Array[AnyRef]())))
+              map += Pair(n, (() => m.invoke(sobj, Array[AnyRef]():_*)))
             }
           }
           gettersCache.put(cls, map)
@@ -143,55 +142,11 @@ class ScalaHashModel(wrapper: ObjectWrapper, sobj: ScalaObject) extends Template
       }
     }
   }
+
   def get(key: String) : TemplateModel = getters.get(key) match {
     case Some(getter) => wrapper.wrap(getter())
     case None => throw new TemplateModelException(key+" not found in object "+sobj)
   }
+
   def isEmpty = false
-}
-
-/** Convert scala.Collection to java.util.Collection.
- * Note that java's iterator' remove method will not be implemented. */
-class JCollection[A](col: scala.Collection[A]) extends java.util.AbstractCollection[A]{
-  def iterator = new java.util.Iterator[A]{
-    val elems = col.elements
-    def hasNext = elems.hasNext
-    def next = elems.next
-    def remove = throw new UnsupportedOperationException("remove")
-  }
-  def size = col.size
-}
-
-/** Campanion class */
-object JCollection{
-  def apply[A](col: scala.Collection[A]) = new JCollection(col)
-}
-
-/** Convert scala.Seq to java.util.List */
-class JList[A](seq: scala.Seq[A]) extends java.util.AbstractList[A] {
-  def get(idx: Int) = seq(idx)
-  def size = seq.size
-}
-/** Campanion class */
-object JList{
-  //def apply[A](seq: scala.Seq[A]) = new JList(seq)
-  def apply[A](tuples: (A)*) = new JList(tuples)
-}
-
-/** A shorter name for java.util.HashMap, but has a campanion object factory. */
-class JMap[A,B] extends java.util.HashMap[A,B]
-
-/** Campanion class the produce java map instance. */
-object JMap{
-  def apply[A, B](tuples: (A,B)*): JMap[A,B] = {
-    val map = new JMap[A,B]
-    for((k,v) <- tuples) map.put(k,v)
-    map
-  }
-  //TODO: we should look more on java.util.AbstractMap to actually implement this like JList.
-  def apply[A, B](imap: scala.collection.Map[A,B]): JMap[A,B] = {
-    val map = new JMap[A,B]
-    for((k,v) <- imap) map.put(k,v)
-    map
-  }
 }
